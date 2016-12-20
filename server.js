7ef4e50773e17d37f464fa330107f9b9d32d78a2
Bruno Salapic <sala4460@mylaurier.ca>
2016-12-20 14:38:52 -0500

    redo the server.js

--- server.js
+++ server.js
@@ -1,51 +1,72 @@
-//basic server
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var express=require("express");
-var bodyparser=require('body-parser');
-var app=express();
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var port=process.env.PORT || 3000;
-var todonextid=1;
-var todos=[];
-app.use(bodyparser.json());
-app.get('/',function(req,res){
-	res.send("todo api root");
+app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
 });
 
-//GET /todos
-// get /todos/id
-app.get('/todos',function(req,res){
+// GET /todos
+app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
 
-app.get('/todos/:id',function(req,res){
-
-		var todoid=parseInt(req.params.id,10);
-		var matchtodo;
-
-		todos.forEach(function(todo){
-			if(todoid===todo.id){
-				matchtodo=todo;
-			}
-		});
-		if(matchtodo){
-			res.json(matchtodo);
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
 		}
-		else{
-			res.status(404).send();
-		}
-		
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
-app.post('/todos',function(req,res){
-	var body=req.body;
-	body.id=todonextid;
-	todonextid++;
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
 	todos.push(body);
+	
 	res.json(body);
 });
 
-app.listen (port,function(){
-	console.log("express listenning on port"+ port+"!");
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
